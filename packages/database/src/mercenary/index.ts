4ae36cbca24a7b2eaec01f4913d8028f479cc269
bangbokirs <burn.design00@gmail.com>
2025-01-22 13:47:56 +0700

    refactor(database): setup properly

--- packages/database/src/mercenary/index.ts
+++ packages/database/src/mercenary/index.ts
@@ -1,8 +1,8 @@
-import { Mercenary } from "@/types/mercenary";
-import * as cnMercenary from "./cn";
-import * as enMercenary from "./en";
-import * as idMercenary from "./id";
-import * as krMercenary from "./kr";
+import { Mercenary } from "@losaweb/database/types/mercenary";
+import * as cnMercenary from "@losaweb/database/mercenary/cn";
+import * as enMercenary from "@losaweb/database/mercenary/en";
+import * as idMercenary from "@losaweb/database/mercenary/id";
+import * as krMercenary from "@losaweb/database/mercenary/kr";
 
 const mercenaryData: Record<string, { mercenary: Mercenary[] }> = {
   cn: cnMercenary,
@@ -14,5 +14,5 @@ const mercenaryData: Record<string, { mercenary: Mercenary[] }> = {
 export default function getMercenaryData(locale: string): Mercenary[] {
   // Default to 'en'!!
   const data = mercenaryData[locale] || mercenaryData["en"];
-  return data.mercenary;
+  return data?.mercenary ?? [];
 }
